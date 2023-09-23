5f9250a2f02d4562732e19c46ce125e62cd0504c
Yash Bhavsar <bhavsary@mcmaster.ca>
2023-09-23 16:45:34 -0400

    Add conditional statement to have Github Icon only when link is present

--- client/src/components/projectInfo.js
+++ client/src/components/projectInfo.js
@@ -5,14 +5,17 @@ const ProjectInfo = ({ project, handleGithubIcon, githubBlack, githubWhite }) =>
             <ul>
                 <li className="project-header">
                     <p className="project-title">{project.title}</p>
-                    <a className="github-icon-container" href={project.attachments}
-                        onMouseEnter={() => handleGithubIcon(project._id)}
-                        onMouseLeave={() => handleGithubIcon(project._id)}>
-                        <img className="github-icon"
-                            src={project.githubIcon === githubWhite ? githubBlack : githubWhite}
-                            alt="github">
-                        </img>
-                    </a>
+                    {project.attachments && project.attachments !== "" && (
+                        <a className="github-icon-container" href={project.attachments}
+                            onMouseEnter={() => handleGithubIcon(project._id)}
+                            onMouseLeave={() => handleGithubIcon(project._id)}>
+                            <img className="github-icon"
+                                src={project.githubIcon === githubWhite ? githubBlack : githubWhite}
+                                alt="github">
+                            </img>
+                        </a>
+                    )}
+
                 </li>
 
                 <li className="jotnotes"><ul>
